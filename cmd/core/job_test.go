2481e3bcfc9473ac1a13506e69ff2280bd729ffb
Xin Zeng <zengxinzhy@gmail.com>
2023-12-11 05:02:54 +1000

    update protobuf for mapper/reducer structure

--- cmd/core/job_test.go
+++ cmd/core/job_test.go
@@ -1,17 +1,11 @@
 package core_test
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
-	"mime/multipart"
-	"net/http"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/go-retryablehttp"
-	"github.com/sath-run/engine/cmd/utils"
 )
 
 // func TestFileUpload(t *testing.T) {
@@ -110,92 +104,4 @@ func TestRequest(t *testing.T) {
 	res, err := retryablehttp.NewClient().Do(req)
 	checkErr(err)
 	spew.Dump(res)
-}
-
-func DoUpload(folder string, method string, url string, params map[string]string) (*http.Response, error) {
-	var buf bytes.Buffer
-	if err := utils.Compress(folder, &buf); err != nil {
-		return nil, err
-	}
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("file", "output.tar.gz")
-	if err != nil {
-		return nil, err
-	}
-	_, err = io.Copy(part, &buf)
-	if err != nil {
-		return nil, err
-	}
-
-	for key, val := range params {
-		_ = writer.WriteField(key, val)
-	}
-	if err := writer.Close(); err != nil {
-		return nil, err
-	}
-	req, err := retryablehttp.NewRequest("PUT", url, body)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	client := retryablehttp.NewClient()
-	resp, err := client.Do(req)
-	return resp, err
-}
-
-func TestUpload(t *testing.T) {
-	// tar + gzip
-	url := "http://localhost:8080/outputs/uploads"
-	params := map[string]string{
-		"title":       "My Document",
-		"author":      "Matt Aimonetti",
-		"description": "A document with all the Go programming language secrets",
-	}
-	resp, err := DoUpload("./output", "PUT", url, params)
-	checkErr(err)
-	data, err := io.ReadAll(resp.Body)
-	checkErr(err)
-	fmt.Println(string(data))
-}
-
-func TestRedirectedUpload(t *testing.T) {
-	url := "http://localhost:8080/outputs"
-	obj := map[string]any{
-		"sathProjectId": "aaa",
-		"sathJobId":     "bbb",
-		"sathExecId":    "ccc",
-	}
-	data, err := json.Marshal(obj)
-	checkErr(err)
-	req, err := retryablehttp.NewRequest("POST", url, data)
-	checkErr(err)
-	req.Header.Set("Content-Type", "application/json")
-	res, err := retryablehttp.NewClient().Do(req)
-	checkErr(err)
-	data, err = io.ReadAll(res.Body)
-	checkErr(err)
-	var response struct {
-		Method string `json:"method"`
-		Url    string `json:"url"`
-	}
-	err = json.Unmarshal(data, &response)
-	checkErr(err)
-	// resp, err := DoUpload("./output", response.Method, response.Url, nil)
-	var buf bytes.Buffer
-	if err := utils.Compress("./output", &buf); err != nil {
-		checkErr(err)
-	}
-	req, err = retryablehttp.NewRequest(response.Method, response.Url, &buf)
-	if err != nil {
-		checkErr(err)
-	}
-	resp, err := retryablehttp.NewClient().Do(req)
-	if err != nil {
-		checkErr(err)
-	}
-	defer resp.Body.Close()
-	data, err = io.ReadAll(resp.Body)
-	checkErr(err)
-	fmt.Println(string(data))
 }
